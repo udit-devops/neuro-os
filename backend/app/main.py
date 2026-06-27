b48de74db236525dcdfa388415b82b198e3df13f
Udit <dakuaudit@gmail.com>
2026-06-27 12:11:59 +0530

    added router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,7 @@
 from fastapi import FastAPI
+from app.api.health.routes import router as health_router
+app  = FastAPI()
+app.include_router(health_router)
+@app.get("/")
+def root():
+    return {"message": "welcome boiss"}
