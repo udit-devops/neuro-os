7bacc75d3f416f8a5383971fb164c38c1b47d3c3
Udit <dakuaudit@gmail.com>
2026-07-01 01:40:10 +0530

    added pydantic basemodel and all for schemas

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 from app.api.health.routes import router as health_router
 from app.core.config import settings
-app  = FastAPI(
+from contextlib import asynccontextmanager
+from app.db.init_db import init_db
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
+
+app = FastAPI(
     title=settings.APP_NAME,
-    debug=settings.DEBUG
+    debug=settings.DEBUG,
+    lifespan=lifespan
 )
 app.include_router(health_router)
+
 @app.get("/")
 def root():
     return {"message": "welcome boiss"}
